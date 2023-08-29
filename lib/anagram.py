e4337a42b3a43d778434cf1ff3a3329cf18dbe0c
Mark Muchai <muchaimark6@gmail.com>
2023-08-29 16:08:26 +0300

    new commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+
+        return [word for word in words if sorted(word.lower()) == sorted(
+            self.word.lower()) and word.lower() != self.word.lower()]
+
+
+listen = Anagram("angel")
+print(listen.match(["enlist", "google", "glean"]))
